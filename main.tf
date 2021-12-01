16eca8a543d2fe2503d99518a8c582a539badb87
wasim30 <wasim_shaikh56@yahoo.com>
2021-12-01 15:10:57 +0530

    Create main.tf
    main.tf file created

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09ce2fc392a4c0fbc"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
