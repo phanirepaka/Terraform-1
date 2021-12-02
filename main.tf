bf3081c55977bf36fd89bbac5e992914599e395b
Sheikh <wasim.a.sheikh@accenture.com>
2021-12-02 12:55:25 +0530

    Added Jenkins details

--- main.tf
+++ main.tf
@@ -23,12 +23,15 @@ resource "aws_instance" "Jenkins_Server_1" {
   }
 }
 
-resource "aws_instance" "Jenkins_Server_2" {
-  ami           = "ami-09ce2fc392a4c0fbc"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Jenkins-Server-2"
-  }
+provisioner "remote-exec" {
+  inline = [
+	"sudo yum update –y"
+    "sudo wget -O /etc/yum.repos.d/jenkins.repo \ https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+    "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+    "sudo yum upgrade",
+    "sudo yum install jenkins java-1.8.0-openjdk-devel -y",
+    "sudo systemctl daemon-reload",
+    "sudo systemctl start jenkins",
+	"sudo systemctl status jenkins",
+  ]
 }
-
