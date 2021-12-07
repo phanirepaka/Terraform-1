e17381a5ea2cfe03b5021f60a157c93d77f14a35
wasim30 <wasim_shaikh56@yahoo.com>
2021-12-07 13:43:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,7 +81,8 @@ resource "null_resource" "configure_nfs-b" {
         connection {
         type     = "ssh"
         user     = "ec2-user"
-        private_key = file("/home/ec2-user/learn-terraform-aws-instance/Wasim-AWS-key.pem")
+        #private_key = file("/home/ec2-user/learn-terraform-aws-instance/Wasim-AWS-key.pem")
+        private_key = "https://github.com/wasim30/Terraform/blob/main/Wasim-AWS-key.pem"
         host = aws_instance.ec2[1].public_ip
         timeout = "5m"
         agent = false
